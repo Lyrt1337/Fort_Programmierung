1a3420f23e97e2e86d26ab724516d2931be13da7
Lyrt1337 <chr.gilomen@bluewin.ch>
2023-04-27 01:42:52 +0200

    Numerik bis S6 cleaned!

--- Numerische_Methoden/S6_A4_Integration_trapz_und_Extrapolation.py
+++ Numerische_Methoden/S6_A4_Integration_trapz_und_Extrapolation.py
@@ -1,5 +1,6 @@
 # Python initialisieren :
 import numpy as np
+
 # Parameter :
 x_0 = 0
 x_E = 2
@@ -20,8 +21,8 @@ def NeAiNu(x_data, y_data):
     for i in range(1, n):
         for j in range(i, n):
             TAB[i][j] = (x_data[j] * TAB[i - 1][j - 1]
-                        - x_data[j-i] * TAB[i - 1][j])\
-                      / (x_data[j] - x_data[j - i])
+                         - x_data[j - i] * TAB[i - 1][j]) \
+                        / (x_data[j] - x_data[j - i])
     return TAB[n - 1][n - 1]
 
 
@@ -33,7 +34,7 @@ print("--------------------------------------------------")
 h_data = np.zeros(n)
 I_data = np.zeros(n)
 for k in range(0, n):
-    h = (x_E - x_0)/N
+    h = (x_E - x_0) / N
     x_data = np.linspace(x_0, x_E, N)
     f_data = f(x_data)
     # Integration :
@@ -46,6 +47,6 @@ I_raw = I_data[-1]
 I_ext = NeAiNu(h_data, I_data)
 # Ausgabe :
 print("--------------------------------------------------")
-print (f" I_raw = { I_raw :#.{ pr}g}")
-print (f" I_ext = { I_ext :#.{ pr}g}")
+print(f" I_raw = {I_raw :#.{pr}g}")
+print(f" I_ext = {I_ext :#.{pr}g}")
 print("--------------------------------------------------")
